d08e89e632522a4e45ae301822f30f37326b5dfc
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-10-17 22:27:18 +0300

    first steps

---
+++ 7.VowelOrConsonant/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _7.VowelOrConsonant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = char.Parse(Console.ReadLine() ?? "A");
+
+            letter = char.ToUpper(letter);
+
+            switch (letter)
+            {
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                    Console.WriteLine("Vowel"); break;
+                default:
+                    Console.WriteLine("Consonant"); break;
+            }
+        }
+    }
+}
